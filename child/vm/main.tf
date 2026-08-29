21e2fd815f4fd1724b90d69e01f49a472118e54b
Abhishek <abhishek3758sharma@gmail.com>
2026-08-30 01:17:51 +0530

    Format Terraform modules

--- child/vm/main.tf
+++ child/vm/main.tf
@@ -1,17 +1,17 @@
 variable "vm" {}
 data "azurerm_subnet" "snet" {
-    for_each = var.vm
+  for_each             = var.vm
   name                 = each.value.snet_name
   virtual_network_name = each.value.virtual_network_name
   resource_group_name  = each.value.resource_group_name
 }
 data "azurerm_public_ip" "pips" {
-    for_each = var.vm
+  for_each            = var.vm
   name                = each.value.pips_name
   resource_group_name = each.value.resource_group_name
 }
 resource "azurerm_network_interface" "example" {
-    for_each = var.vm
+  for_each            = var.vm
   name                = each.value.nic_name
   location            = each.value.location
   resource_group_name = each.value.resource_group_name
@@ -24,13 +24,13 @@ resource "azurerm_network_interface" "example" {
   }
 }
 resource "azurerm_linux_virtual_machine" "linux_vm" {
-    for_each = var.vm
-  name                = each.value.vm_name
-  resource_group_name = each.value.resource_group_name
-  location            = each.value.location
-  size                = each.value.size
-  admin_username      = each.value.admin_username
-  admin_password = each.value.admin_password
+  for_each                        = var.vm
+  name                            = each.value.vm_name
+  resource_group_name             = each.value.resource_group_name
+  location                        = each.value.location
+  size                            = each.value.size
+  admin_username                  = each.value.admin_username
+  admin_password                  = each.value.admin_password
   disable_password_authentication = false
   network_interface_ids = [
     #EMPLICIT DEPENDENCY LAGAYA GAYA HAI KYU KI PHALE NIC BANE GA USKE BAAD VM
